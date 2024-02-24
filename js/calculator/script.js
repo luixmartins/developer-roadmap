92e058ea5ad03335068fae57a22442584439cc6e
luixmartins <luizmartins.uemg@gmail.com>
2024-02-24 19:09:47 -0300

    Initial commit on JS calculator

---
+++ js/calculator/script.js
@@ -0,0 +1,10 @@
+function writeNumber(value) {
+    var input = document.getElementById('input')
+    var text_input = input.value
+    
+    for (i = 0; i <= text_input.length; i++){
+        
+    }
+
+    input.value +=  value
+}
