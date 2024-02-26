536f5d446e5b10774fd8607474a1e28d6ecb8f4b
luixmartins <luizmartins.uemg@gmail.com>
2024-02-25 21:06:29 -0300

    Created function operation on js file

--- js/calculator/script.js
+++ js/calculator/script.js
@@ -1,10 +1,47 @@
 function writeNumber(value) {
-    var input = document.getElementById('input')
-    var text_input = input.value
+    const input = document.getElementById('input')
     
-    for (i = 0; i <= text_input.length; i++){
-        
+    if (value === ","){
+        if (input.value.slice(-1) !== ","){ 
+            input.value = input.value.replace(",", "")
+            input.value += ","
+        }
+    } else { 
+        input.value += value 
+    }
+}
+
+function operation(operator){ 
+    const input = document.getElementById('input')
+
+    if (operator !== "=" && input.value != ""){
+        const firstValue = parseFloat(input.value) 
+        const operatorValue = operator 
+
+        input.value = ""
+    } else {
+        if (input.value != ""){ 
+            const secondValue = parseFloat(input.value)  
+        }
     }
 
-    input.value +=  value
+    if (operator === "="){ 
+        switch (operatorValue){ 
+            case "+": {
+                result = firstValue + secondValue
+            }
+            case "-": {
+                result = firstValue - secondValue 
+            }
+            case "*": {
+                result = firstValue * secondValue 
+            }
+            case "/": {
+                result = firstValue / secondValue 
+            }
+        }
+
+        input.value = result 
+    }
+
 }
